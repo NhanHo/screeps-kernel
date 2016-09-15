657503cd00cfca1161d57cbaa2e60c55344b74a5
Nhan Ho <human@nhanho.me>
2016-09-15 21:53:49 +0700

    Add abstract process

---
+++ kernel/process.ts
@@ -0,0 +1,31 @@
+import { killProcess } from "../kernel/kernel";
+import { ProcessPriority } from "./constants";
+import { ProcessStatus } from "./process-status";
+import { ProcessSleep } from "../typings/process-sleep";
+abstract class Process {
+    public status: number;
+    public classPath: string;
+    public sleepInfo?: ProcessSleep;
+    public priority: ProcessPriority;
+    public memory: any;
+
+    constructor(public pid: number,
+        public parentPID: number,
+        priority = ProcessPriority.LowPriority) {
+
+        this.status = ProcessStatus.ALIVE;
+        this.priority = priority;
+    };
+
+    public abstract run(): number;
+    public setMemory(memory: any): void {
+        this.memory = memory;
+    };
+
+    public stop(signal: number) {
+        killProcess(this.pid);
+        return signal;
+    }
+}
+
+export = Process;
