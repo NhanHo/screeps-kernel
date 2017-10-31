d9df0cc05b54433e095ff656bd384ac44a861ea8
East Agile <eastagile@Easts-iMac.local>
2017-10-31 14:41:05 +0700

    Update

--- kernel/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-(function (ProcessPriority) {
-    ProcessPriority[ProcessPriority["Ticly"] = 1] = "Ticly";
-    ProcessPriority[ProcessPriority["TiclyLast"] = 2] = "TiclyLast";
-    ProcessPriority[ProcessPriority["LowPriority"] = 3] = "LowPriority";
-})(exports.ProcessPriority || (exports.ProcessPriority = {}));
-var ProcessPriority = exports.ProcessPriority;
--- kernel/process-status.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.ProcessStatus = {
-    DEAD: -1,
-    ALIVE: 0,
-    SLEEP: 1,
-};
